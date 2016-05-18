d630681dbf881079a6ee3e9aa41bc54d7f10048a
aleyds <xiaoxingxingyds@sina.com>
2016-05-18 08:31:54 +0800

    工程名修改

--- RTE/RTE_Components.h
+++ RTE/RTE_Components.h
@@ -3,8 +3,8 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: '5182LockProject' 
- * Target:  'nrf5182' 
+ * Project: 'Keyboard' 
+ * Target:  'Keyboard' 
  */
 
 #ifndef RTE_COMPONENTS_H
